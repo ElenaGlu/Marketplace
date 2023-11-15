ecdf5f07ef2ebf535a2f68f1aa40d1e4e7156663
ElenaGlu <shishalovae@mail.ru>
2023-11-15 20:18:31 +0500

    added models

--- MarketPlace/buyer/models.py
+++ MarketPlace/buyer/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Emails(models.Model):
+    email = models.CharField(max_length=150)
+
+
+class Profile(models.Model):
+    BUYER_PROFILE = 1
+    SELLER_PROFILE = 2
+    PROFILE_CHOICES = (
+        (BUYER_PROFILE, 'Buyer'),
+        (SELLER_PROFILE, 'Seller'),
+    )
+    name = models.CharField(max_length=150)
+    surname = models.CharField(max_length=150)
+    password = models.CharField(max_length=150)
+    email = models.ForeignKey(Emails, on_delete=models.CASCADE)
+    profile = models.IntegerField(choices=PROFILE_CHOICES)
--- MarketPlace/buyer/views.py
+++ MarketPlace/buyer/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
+def register(request):
+    pass
 
-# Create your views here.
