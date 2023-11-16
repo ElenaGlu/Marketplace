f3c71801b12fed0e4716466fa49755312cb9d33d
ElenaGlu <shishalovae@mail.ru>
2023-11-16 17:53:29 +0500

    update logic of models

--- MarketPlace/seller/models.py
+++ MarketPlace/seller/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django_countries.fields import CountryField
 
-# Create your models here.
+from buyer.models import Emails
+
+
+class ProfileSeller(models.Model):
+    TYPE_ORGANIZATION = (
+        ('ИП', 'Индивидуальный предприниматель'),
+        ('ООО', 'Общество с Ограниченной Ответственностью'),
+    )
+    Individual_Taxpayer_Number = models.CharField(max_length=12)
+    type_of_organization = models.IntegerField(choices=TYPE_ORGANIZATION)
+    country_of_registration = CountryField()
+    password = models.CharField(max_length=150)
+    email = models.ForeignKey(Emails, on_delete=models.CASCADE)
